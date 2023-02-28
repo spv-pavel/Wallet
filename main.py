a54d61d1cfe2ad38c5d3ddac47c3534031875e80
Pavel Spichakov <spv-pavel@yandex.ru>
2023-02-28 20:50:27 +0300

    Card: change_card

--- main.py
+++ main.py
@@ -18,17 +18,29 @@ class Wallet(ABC):
 
 
 class CreditCard(Wallet):
-    def __init__(self, name, limit=1000):
+    def __init__(self, name, limit=-1000):
         self.limit = limit
         super().__init__(name)
 
     def change_balance(self, value: int):
-        ...
+        if self.balance + value < self.limit:
+            print(f'Not enough balance {self.balance}')
+        else:
+            self.balance += value
 
 
 class Card(Wallet):
     def __init__(self, name):
         super().__init__(name)
+
+    def change_card(self):
+        if self.balance < 100:
+            print(f'Not enough balance {self.balance}')
+        else:
+            self.balance -= 100
+            _card = ProCard(self.name)
+            _card.balance = self.balance
+            return _card
 
 
 class ProCard(Wallet):
@@ -42,11 +54,16 @@ class ProCard(Wallet):
             self.balance += value * 0.95 if self.balance + value * 0.95 < self.balance else value
 
 
-card = ProCard('Sam')
+card = Card('Sam')
 print(card.get_balance())
 card.change_balance(1000)
 print(card.get_balance())
 card.change_balance(-800)
 print(card.get_balance())
 card.change_balance(-300)
+print(card.get_balance())
+print(card.type)
+card = card.change_card()
+print(card.type)
+card.change_balance(-100)
 print(card.get_balance())
